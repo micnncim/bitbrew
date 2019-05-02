5c33555e80f4c5a4c85a1c94ccceb114015dd8db
micnncim <micnncim@gmail.com>
2019-05-02 12:54:18 +0900

    Add config

---
+++ config/config.go
@@ -0,0 +1,133 @@
+package config
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	*BitBar
+	*GitHub
+}
+
+type BitBar struct {
+	FormulaPath  string `mapstructure:"formula_path"`
+	PluginFolder string `mapstructure:"plugin_folder"`
+}
+
+type GitHub struct {
+	Token string
+}
+
+var (
+	newDefaultViperFunc = newDefaultViper
+	initConfigFunc      = initConfig
+	configDirFunc       = configDir
+	editFunc            = Edit
+)
+
+var (
+	defaultConfigDir  = ".config/bitbrew"
+	defaultConfigName = "config.yaml"
+)
+
+func New() (*Config, error) {
+	configDir, err := configDirFunc()
+	if err != nil {
+		return nil, err
+	}
+	conf, err := initConfigFunc(configDir)
+	if err != nil {
+		return nil, err
+	}
+	return conf, nil
+}
+
+func newDefaultViper(configDir string) (*viper.Viper, error) {
+	v := viper.New()
+
+	v.SetConfigType("yaml")
+	v.SetConfigName("config")
+	v.AddConfigPath(configDir)
+
+	v.SetDefault("bitbar.formula_path", filepath.Join(configDir, "formula"))
+	v.SetDefault("bitbar.plugin_folder", filepath.Join(configDir, "plugins"))
+
+	return v, nil
+}
+
+func initConfig(configDir string) (*Config, error) {
+	v, err := newDefaultViperFunc(configDir)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := v.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			if err := os.MkdirAll(configDir, 0755); err != nil {
+				return nil, err
+			}
+			if err := v.WriteConfigAs(filepath.Join(configDir, defaultConfigName)); err != nil {
+				return nil, err
+			}
+			if err := editFunc(); err != nil {
+				return nil, err
+			}
+		} else {
+			return nil, err
+		}
+	}
+
+	var c Config
+	if err := v.Unmarshal(&c); err != nil {
+		return nil, err
+	}
+	return &c, nil
+}
+
+func configDir() (string, error) {
+	home, err := homedir.Dir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(home, defaultConfigDir), nil
+}
+
+func Edit() error {
+	editor := getEditor()
+	if editor == "" {
+		return errors.New("require $EDITOR or vim")
+	}
+	configDir, err := configDir()
+	if err != nil {
+		return err
+	}
+	return runEditor(editor, filepath.Join(configDir, defaultConfigName))
+}
+
+func getEditor() string {
+	if env := os.Getenv("EDITOR"); env != "" {
+		return env
+	}
+	p, err := exec.LookPath("vim")
+	if err != nil {
+		return ""
+	}
+	return p
+}
+
+func runEditor(editor, path string) error {
+	cmd := exec.Command(editor, path)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return errors.Wrapf(err, "failed to execute %s", editor)
+	}
+	return nil
+}
---
+++ config/export_test.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+var (
+	ExportInitConfigFunc = initConfigFunc
+)
+
+func ExportSetDefaultConfigName(s string) (resetFunc func()) {
+	var org string
+	org, defaultConfigName = defaultConfigName, s
+	return func() {
+		defaultConfigName = org
+	}
+}
+
+func ExportSetInitConfigFunc(f func(string) (*Config, error)) (resetFunc func()) {
+	var org func(string) (*Config, error)
+	org, initConfigFunc = initConfigFunc, f
+	return func() {
+		initConfigFunc = org
+	}
+}
+
+func ExportSetNewDefaultViperFunc(f func(string) (*viper.Viper, error)) (resetFunc func()) {
+	var org func(string) (*viper.Viper, error)
+	org, newDefaultViperFunc = newDefaultViperFunc, f
+	return func() {
+		newDefaultViperFunc = org
+	}
+}
+
+func ExportSetEditFunc(f func() error) (resetFunc func()) {
+	var org func() error
+	org, editFunc = editFunc, f
+	return func() {
+		editFunc = org
+	}
+}
