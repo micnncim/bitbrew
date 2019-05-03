1c51531437954bf8d3749f6bd33d4aed5d2d3d81
micnncim <micnncim@gmail.com>
2019-05-03 19:23:30 +0900

    Add sync command

--- cli/cmd/sync.go
+++ cli/cmd/sync.go
@@ -1,9 +1,41 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/urfave/cli"
+
+	"github.com/micnncim/bitbrew/cli/ui"
+	"github.com/micnncim/bitbrew/config"
+	"github.com/micnncim/bitbrew/di"
 )
 
 func Sync(c *cli.Context) error {
+	s := ui.NewSpinner("Syncing...")
+	s.Start()
+	defer s.Stop()
+
+	conf, err := config.New()
+	if err != nil {
+		return err
+	}
+
+	bitbrew, err := di.InitBitBrew(conf.GitHub.Token, conf.BitBar.FormulaPath, conf.BitBar.PluginFolder)
+	if err != nil {
+		return err
+	}
+
+	installed, uninstalled, err := bitbrew.Sync()
+	if err != nil {
+		return err
+	}
+	fmt.Println()
+	for _, p := range installed {
+		ui.Infof("✔ %s installed!\n", p.Filename)
+	}
+	for _, p := range uninstalled {
+		ui.Warnf("✔ %s uninstalled!\n", p.Filename)
+	}
+
 	return nil
 }
