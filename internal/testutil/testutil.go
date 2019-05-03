92afbc82fda250294e3965e12ab9616b6eed0e59
micnncim <micnncim@gmail.com>
2019-05-03 19:27:17 +0900

    Update testutil

--- internal/testutil/testutil.go
+++ internal/testutil/testutil.go
@@ -11,7 +11,7 @@ import (
 )
 
 func Mkdir(t *testing.T, dir string) (cleanup func()) {
-	err := os.Mkdir(dir, 0755)
+	err := os.MkdirAll(dir, 0755)
 	require.NoError(t, err)
 	return func() {
 		err := os.RemoveAll(dir)
@@ -22,6 +22,12 @@ func Mkdir(t *testing.T, dir string) (cleanup func()) {
 func RemoveAll(t *testing.T, dir string) {
 	err := os.RemoveAll(dir)
 	require.NoError(t, err)
+}
+
+func ReadDir(t *testing.T, dir string) []os.FileInfo {
+	infos, err := ioutil.ReadDir(dir)
+	require.NoError(t, err)
+	return infos
 }
 
 func WriteFile(t *testing.T, path string, buf []byte) (cleanup func()) {
@@ -57,6 +63,11 @@ func CopyFile(t *testing.T, srcName, dstName string) (cleanup func()) {
 		err := os.Remove(dstName)
 		require.NoError(t, err)
 	}
+}
+
+func IsExists(t *testing.T, path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
 }
 
 func NormalizeTestName(name string) string {
